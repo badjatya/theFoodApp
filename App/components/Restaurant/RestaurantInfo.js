40f8c5ab8f0c6d142c02ca76600860857e93bb10
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-06 19:12:31 +0530

    RestaurantInfo styling

--- App/components/Restaurant/RestaurantInfo.js
+++ App/components/Restaurant/RestaurantInfo.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const RestaurantInfo = () => {
+const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
   return (
     <View>
-      <Text>RestaurantInfo</Text>
+      <Text>{name}</Text>
     </View>
   );
 };
