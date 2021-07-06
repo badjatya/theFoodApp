fa463506e046b9234609fb0f4047aad43b82fec5
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-07 00:55:46 +0530

    Using styledcomponents themeProvider

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 
+// IMPORTING ThemeProvider from styledComponents
+import { ThemeProvider } from "styled-components";
+
+// IMPORTING Custom Theme
+import theme from "./App/Theme/Theme";
+
 // IMPORTING Screens
 import RestaurantScreen from "./App/screens/Restaurant/RestaurentScreen";
 
 export default function App() {
   return (
     <>
-      <RestaurantScreen />
+      <ThemeProvider>
+        <RestaurantScreen theme={theme} />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
