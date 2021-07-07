6a9b4d8f1b795144d732f4cd702bfb5df0ceb9cf
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-07 21:44:00 +0530

    RestaurantCard styles

--- App/components/Restaurant/RestaurantInfoCard.js
+++ App/components/Restaurant/RestaurantInfoCard.js
@@ -4,9 +4,6 @@ import { Image, Text, View } from "react-native";
 // IMPORTING Styled-components
 import styled from "styled-components/native";
 
-//IMPORTING Components
-// import { Spacer } from "../spacers/spacer";
-
 // IMPORTING react-native-paper
 import { Card } from "react-native-paper";
 
@@ -16,49 +13,6 @@ import { SvgXml } from "react-native-svg";
 // IMPORTING SVG
 import starSvg from "../../assets/images/star";
 import openIcon from "../../assets/images/open";
-
-// Styles
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  background-color: ${(props) => props.theme.colors.ui.quaternary};
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-  padding-top: 0;
-`;
-
-const Title = styled.Text`
-  color: ${(props) => props.theme.colors.ui.primary};
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
-
-const Address = styled.Text`
-  color: ${(props) => props.theme.colors.ui.primary};
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fontSizes.caption};
-`;
 
 // NOTE RestaurantInfoCard
 const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -107,5 +61,48 @@ const RestaurantInfoCard = ({ restaurant = {} }) => {
     </RestaurantCard>
   );
 };
+
+// Styles
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  background-color: ${(props) => props.theme.colors.ui.quaternary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+  padding-top: 0;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
+
+const Address = styled.Text`
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
 
 export default RestaurantInfoCard;
