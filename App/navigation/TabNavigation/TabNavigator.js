6c9576702987c5053dc5888073aea314d360721f
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-22 21:58:30 +0530

    Refracting Navigation

--- App/navigation/TabNavigation/TabNavigator.js
+++ App/navigation/TabNavigation/TabNavigator.js
@@ -11,28 +11,30 @@ import SettingScreen from "../../screens/settings/SettingScreen";
 // IMPORTING ICONS
 import { Ionicons } from "@expo/vector-icons";
 
+// Icons
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+
+// Screen Options
+const createScreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
+
 // NOTE BottomTab
 const TabNavigator = createBottomTabNavigator();
 
 const TabNavigatorScreen = () => {
   return (
     <TabNavigator.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName;
-
-          if (route.name === "Restaurants") {
-            iconName = "md-restaurant";
-          } else if (route.name === "Settings") {
-            iconName = "md-settings";
-          } else if (route.name === "Map") {
-            iconName = "md-map";
-          }
-
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
+      screenOptions={createScreenOptions}
       tabBarOptions={{
         activeTintColor: "tomato",
         inactiveTintColor: "gray",
