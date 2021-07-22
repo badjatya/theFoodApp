75a46f88b05cbb487eaa4af6dda6d3e2c573a8b9
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-23 03:43:14 +0530

    Styled RestaurantDetailScreen

--- App/screens/Restaurant/RestaurantDetailScreen.js
+++ App/screens/Restaurant/RestaurantDetailScreen.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, ScrollView, StyleSheet } from "react-native";
+import { List } from "react-native-paper";
 
 // Importing Components
 import RestaurantInfoCard from "../../components/Restaurant/RestaurantInfoCard";
@@ -7,13 +8,71 @@ import RestaurantInfoCard from "../../components/Restaurant/RestaurantInfoCard";
 const RestaurantDetailScreen = (props) => {
   const { route } = props;
   const { restaurant } = route;
+
+  // State
+  const [breakfastExpanded, setBreakfastExpanded] = useState(false);
+  const [lunchExpanded, setLunchExpanded] = useState(false);
+  const [dinnerExpanded, setDinnerExpanded] = useState(false);
+  const [drinksExpanded, setDrinksExpanded] = useState(false);
+
   return (
     <SafeAreaView>
       <RestaurantInfoCard restaurant={restaurant} />
+
+      <ScrollView style={styles.scroll}>
+        <List.Accordion
+          title="Breakfast"
+          left={(prop) => <List.Icon {...prop} icon="bread-slice" />}
+          expanded={breakfastExpanded}
+          onPress={() => setBreakfastExpanded(!breakfastExpanded)}
+        >
+          <List.Item title="Eggs Benedict" />
+          <List.Item title="Classic Breakfast" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Lunch"
+          left={(prop) => <List.Icon {...prop} icon="hamburger" />}
+          expanded={lunchExpanded}
+          onPress={() => setLunchExpanded(!lunchExpanded)}
+        >
+          <List.Item title="Burger w/ Fries" />
+          <List.Item title="Steak Sandwich" />
+          <List.Item title="Mushroom Soup" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Dinner"
+          left={(prop) => <List.Icon {...prop} icon="food-variant" />}
+          expanded={dinnerExpanded}
+          onPress={() => setDinnerExpanded(!dinnerExpanded)}
+        >
+          <List.Item title="Spaghetti Bolognese" />
+          <List.Item title="Veal Cutlet with Chicken Mushroom Rotini" />
+          <List.Item title="Steak Frites" />
+        </List.Accordion>
+
+        <List.Accordion
+          title="Drinks"
+          left={(prop) => <List.Icon {...prop} icon="cup" />}
+          expanded={drinksExpanded}
+          onPress={() => setDrinksExpanded(!drinksExpanded)}
+        >
+          <List.Item title="Coffee" />
+          <List.Item title="Tea" />
+          <List.Item title="Modelo" />
+          <List.Item title="Coke" />
+          <List.Item title="Fanta" />
+        </List.Accordion>
+      </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default RestaurantDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scroll: {
+    marginBottom: 200,
+  },
+});
