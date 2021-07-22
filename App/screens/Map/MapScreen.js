6a3e75fa8f0f9094eec75b926e73e17044176570
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-23 03:58:22 +0530

    Map Screen Styled

--- App/screens/Map/MapScreen.js
+++ App/screens/Map/MapScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+// Importing Maps
+import MapView from "react-native-maps";
+
 const MapScreen = () => {
   return (
     <View>
-      <Text>MapScreen</Text>
+      <MapView style={styles.map} />
     </View>
   );
 };
 
 export default MapScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: "100%",
+    width: "100%",
+  },
+});
