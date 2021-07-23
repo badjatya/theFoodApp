5010f13e38c023d7d1341046286b1c7c58388fdf
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-23 05:57:51 +0530

    Map Screen Styled

---
+++ App/components/MapCallout/MapCallout.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Importing Components
+import MapRestaurantInfo from "../MapRestaurantInfo/MapRestaurantInfo";
+
+const MapCallout = ({ restaurant }) => (
+  <MapRestaurantInfo restaurant={restaurant} />
+);
+
+export default MapCallout;
--- App/screens/Map/MapScreen.js
+++ App/screens/Map/MapScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 
 // Importing Maps
@@ -6,12 +6,56 @@ import MapView from "react-native-maps";
 
 // Importing Components
 import MapSearch from "../../components/MapSearch/MapSearch";
+import MapCallout from "../../components/MapCallout/MapCallout";
+
+// Importing Context
+import { LocationContext } from "../../services/location/location.context";
+import { RestaurantsContext } from "../../services/restaurant/restaurant.context";
 
 const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
   return (
     <View>
       <MapSearch />
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            >
+              <MapView.Callout>
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
+          );
+        })}
+      </MapView>
     </View>
   );
 };
